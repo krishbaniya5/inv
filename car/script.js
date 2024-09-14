ca85ad78b598f83a065b0aa1d5f511bc2cb38f5e
krishbaniya5 <43073182+krishbaniya5@users.noreply.github.com>
2024-09-14 16:30:49 +0545

    Add files via upload

---
+++ car/script.js
@@ -0,0 +1,32 @@
+// script.js
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".prev").addEventListener("click", function() {
+        plusSlides(-1);
+    });
+    document.querySelector(".next").addEventListener("click", function() {
+        plusSlides(1);
+    });
+});
